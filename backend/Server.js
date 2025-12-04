a086ce058dd494aba207919409314553a511bd33
Aakash Saini <sainiaakash177@gmail.com>
2025-12-04 15:23:45 +0530

    Update CORS config for Netlify

--- backend/Server.js
+++ backend/Server.js
@@ -24,6 +24,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://mmsc.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
