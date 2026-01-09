e47555ea05830ee2dc05e166b59292bd3a0fd438
Aakash Saini <sainiaakash177@gmail.com>
2026-01-09 11:18:36 +0530

    remove vercel

--- backend/Server.js
+++ backend/Server.js
@@ -40,7 +40,6 @@ app.use(
       const allowedOrigins = [
         "http://localhost:5173",
         "https://mmsc.netlify.app",
-        "https://mmsc-ten.vercel.app",
       ];
 
       if (!origin || allowedOrigins.includes(origin)) {
